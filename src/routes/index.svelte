b1d64698434f9511d4aca1c8f8a880b2cd3d921f
Taraobst <taraobersteller21@gmail.com>
2022-08-04 10:00:07 +0200

    taras first moves

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,10 @@
+<script>
+	let name= "tara"
+</script>
+
 <h1>Welcome to Si Drei!</h1>
 <p>24768 Rendsburg Nienstadtstraße 7</p>
-
+<p class="tara">{name} ist cool!</p>
 <style>
 	:global(body) {
 		display: flex;
@@ -13,5 +17,8 @@
 	p {
 		color: coral;
 		margin: 0 auto;
+	}
+	.tara {
+		color: blanchedalmond;
 	}
 </style>
